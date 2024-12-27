d1bc6803c6b04d8c39bf576526a9982803c86ce4
Celiesiene <m.celiesiene@gmail.com>
2024-12-27 11:41:52 +0200

    papildymas

---
+++ Simonos_darbai/slides/app.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.card');
+
+if(slides.length > 0) {
+    slides[slideIndex].classList.add('active');
+    slideIndex++
+}
+
+function showSlides(slideIndex){
+    slides.forEach((slide, i) =>{
+        slide.classList.remove('active');
+    });
+    slides[slideIndex].classList.add('active')
+}
+
+setInterval(function(){
+    if(slideIndex >= slides.length){
+        slideIndex = 0;
+    }
+    showSlides(slideIndex);
+    slideIndex++;
+}, 3000 );
