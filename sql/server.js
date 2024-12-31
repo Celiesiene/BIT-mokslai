56a22286cf773a8756f4848a8b6bab3ae4269677
Celiesiene <m.celiesiene@gmail.com>
2024-12-31 08:16:38 +0200

    papildymas

--- sql/server.js
+++ sql/server.js
@@ -43,6 +43,82 @@ con.query(sql, (err, data) => {
 
 });
 
+app.post('/create', (req, res) => {
+ 
+    // INSERT INTO table_name (column1, column2, column3, ...)
+    // VALUES (value1, value2, value3, ...);
+
+
+    //Daryti saugiai nes galima injekcija
+ 
+    const sql = `
+        INSERT INTO trees (name, height, type)
+        VALUES (?, ?, ?) 
+    `;
+ //klaustukai prie values nes apacioje i masyva irasem name, height ir type. kad neiseitu nulauzti su pvz 123 or 1
+
+    con.query(sql, [req.body.name, req.body.height, req.body.type], (err) => {
+        if (err) {
+            res.send('Klaida įrašant duomenis');
+            return;
+        }
+        res.send('OK');
+    });
+ 
+});
+
+// DELETE FROM table_name WHERE condition;
+//DELETE FROM trees WHERE id = 123 OR 1; 
+
+app.delete('/delete/:id', (req, res) => {
+   
+    // const sql = `
+    //     DELETE FROM trees
+    //     WHERE id = ${req.params.id}
+    // `;
+ 
+    // con.query(sql, (err) => {
+    //     if (err) {
+    //         res.send('Klaida trinant duomenis');
+    //         return;
+    //     }
+    //     res.send('OK');
+    // });
+
+
+
+    const sql = `
+        DELETE FROM trees
+        WHERE id = ?
+    `;
+ 
+    con.query(sql, [req.params.id], (err) => {
+        if (err) {
+            res.send('Klaida trinant duomenis');
+            return;
+        }
+        res.send('OK');
+    });
+ 
+});
+
+app.put('/update/:id', (req, res) => {
+ 
+    const sql = `
+        UPDATE trees
+        SET height = ?, name = ?
+        WHERE id = ?
+    `;
+ 
+    con.query(sql, [req.body.height, req.body.name, req.params.id], (err) => {
+        if (err) {
+            res.send('Klaida atnaujinant duomenis');
+            return;
+        }
+        res.send('OK');
+    });
+ 
+});
 
 
 
