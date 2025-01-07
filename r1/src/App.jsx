25849190bc504d5499ef8f0558e8c7ed1cd2df49
Celiesiene <m.celiesiene@gmail.com>
2025-01-07 16:16:16 +0200

    papildymas

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,12 +1,18 @@
 import './app.css';
 import './buttons.scss'
-import ButtonsTest from './Components/ButtonsTest';
+import B043 from './Components/B043';
+// import ButtonsTest from './Components/ButtonsTest';
+import C043 from './Components/C043';
+import {useState} from 'react';
  
 function App() {
+    const [size, setSize] = useState(100);
   return (
     <div className="app">
       <header className="app-header">
-    <ButtonsTest/>
+    {/* <ButtonsTest/> */}
+    <B043 setSize={setSize}/>
+    <C043 size={size}/>
       </header>
     </div>
   );
---
+++ r1/src/Components/C043.jsx
@@ -0,0 +1,14 @@
+export default function C043({size}) {
+    return(
+        <div style={{
+            width: size + 'px',
+            height: size + 'px',
+            backgroundColor: 'crimson',
+            margin: '20px',
+            transition: 'all 0.3',
+            borderRadius: '50%',
+        }}>
+
+        </div>
+    );
+}
